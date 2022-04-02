f115de65c12c9cb4e2988dd5c058a6c96aa5f826
dbpl566 <deblymon@udel.edu>
2022-04-01 22:37:21 -0400

    Added smooth scroll to View Work button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {HiArrowNarrowRight} from 'react-icons/hi';
+import {Link, animateScroll as scroll} from 'react-scroll';
+import Projects from "./Projects";
 
 const Home = () => {
   return (
@@ -10,14 +12,14 @@ const Home = () => {
             <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Debra Lymon</h1>
             <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Software Developer.</h2>
             <p className='text-[#8892b0] py-4 max-w-[700px]'>I specialize in building well-designed, user-centric applications.</p>
-            <div>
+            <Link to="Projects" smooth={true}>
                 <button className='text-white border-2 px-6 py-3 my-2 flex items-center hover:border-pink-600'>
                   View Work
                   <span className='group-hover:rotate-90 duration-300'>
                   <HiArrowNarrowRight className='ml-3' />
                   </span>
                   </button>
-            </div>
+            </Link>
         </div>
     </div>
   )
