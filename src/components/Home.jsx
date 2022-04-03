08a1ba4f5dba1c0953054c7b2ae9493f974bbe7d
dbpl566 <deblymon@udel.edu>
2022-04-03 18:50:33 -0400

    add sm breakpoints to padding

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ import Projects from "./Projects";
 
 const Home = () => {
   return (
-    <div name='Home' className='w-full h-screen bg-[#030200] py-16'>
+    <div name='Home' className='w-full h-screen bg-[#030200] sm:py-16'>
         {/* Container */}
         <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
             <p className='text-pink-600'>Hi, my name is</p>
