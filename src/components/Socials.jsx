9251050ee8ee63125262efbab132bb3bf78247b4
dbpl566 <deblymon@udel.edu>
2022-04-03 01:20:33 -0400

    sticky navbar

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -4,7 +4,7 @@ import { HiOutlineMail } from 'react-icons/hi';
 
 const Socials = () => {
   return (
-    <div name='Socials' className='w-full h-48 bg-[#030200] flex justify-center items-center pt-8'>
+    <div name='Socials' className='w-full h-48 bg-[#030200] flex justify-center items-center pt-16'>
     <div className='mt-3 flex items-center justify-center space-x-6 text-gray-300 text-4xl'>
       <a className='hover:bg-pink-600' href=""><FaLinkedin /></a>
       <a className='hover:bg-pink-600' href="https://github.com/dbpl566"><FaGithub /></a>
