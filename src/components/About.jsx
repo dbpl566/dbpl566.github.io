0c39c2f6911b24328b1a51fffe645615120fe7b8
dbpl566 <deblymon@udel.edu>
2022-04-03 21:41:47 -0400

    md breakpoint contact

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -22,6 +22,8 @@ const About = () => {
                   <div className='text-gray-300'>
                     <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
                     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                     </p>
                   </div>
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,11 +4,11 @@ import { HiOutlineMail } from 'react-icons/hi';
 
 const Contact = () => {
   return (
-    <div name='Contact' className='w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'>
-    <form method='POST' action="/" className='flex flex-col max-w-[600px] w-full'>
+    <div name='Contact' className='w-full md:h-screen bg-[#0a192f] flex flex-col justify-center items-center p-4'>
+    <form method='POST' action="/" className='flex flex-col max-w-[600px] w-full p-4'>
         <div className='pb-8'>
             <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
-            <p className='text-gray-300 py-4'>// Submit the form below or shoot me an email - myemail@gmail.com</p>
+            <p className='text-gray-300 py-4'>Submit the form below or send me an email - debra.p.lymon@gmail.com</p>
         </div>
         <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
         <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
