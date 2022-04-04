3b8fe5e27f8e82ed45b2c7f80cc503c25e232d8e
dbpl566 <deblymon@udel.edu>
2022-04-03 22:05:38 -0400

    adding h-full to about

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ import { HiOutlineMail } from 'react-icons/hi';
 const Contact = () => {
   return (
     <div name='Contact' className='w-full md:h-screen bg-[#0a192f] flex flex-col justify-center items-center p-4'>
-    <form method='POST' action="/" className='flex flex-col max-w-[600px] w-full p-4'>
+    <form method='POST' action="/" className='flex flex-col max-w-[600px] w-full h-full p-4'>
         <div className='pb-8'>
             <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
             <p className='text-gray-300 py-4'>Submit the form below or send me an email - debra.p.lymon@gmail.com</p>
