b72e68a3c48565d34a6301cf3b9789f051cd643d
dbpl566 <deblymon@udel.edu>
2022-04-18 15:38:56 -0400

    submit button color change

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -6,12 +6,12 @@ const Socials = () => {
   return (
     <div name='Socials' className='w-full h-[150px] bg-[#030200] flex flex-col justify-center items-center'>
       <div className='mt-3 flex justify-center items-center space-x-6 text-gray-300 text-4xl pb-4'>
-        <a className='hover:bg-pink-600' href=""><FaLinkedin /></a>
-        <a className='hover:bg-pink-600' href="https://github.com/dbpl566"><FaGithub /></a>
+        <a className='hover:text-pink-600' href=""><FaLinkedin /></a>
+        <a className='hover:text-pink-600' href="https://github.com/dbpl566"><FaGithub /></a>
       </div>
       <div className='text-gray-300 flex justify-center items-center'>
         <p>
-          Created by Debra Lymon
+          Debra Lymon 2022
         </p>
       </div>
     </div>
