92c431b14722d0023bfecb613c5053077adb9007
dbpl566 <deblymon@udel.edu>
2022-04-22 00:26:57 -0400

    changed project descriptions

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,14 +16,6 @@ const Projects = () => {
     const [info3, setInfo3] = useState(false)
     const handleClick3 = () => setInfo3(!info3)
 
-    const [info4, setInfo4] = useState(false)
-    const handleClick4 = () => setInfo4(!info4)
-
-    const [info5, setInfo5] = useState(false)
-    const handleClick5 = () => setInfo5(!info5)
-
-    const [info6, setInfo6] = useState(false)
-    const handleClick6 = () => setInfo6(!info6)
     return (
         <div name='Projects' className='flex w-full md:h-screen bg-[#030200] text-gray-300 pt-16'>
             <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
@@ -56,7 +48,7 @@ const Projects = () => {
                         </div>
 
                         {/* Project 1 Overlay */}
-                        <div id='overlay-1' className='overflow:hidden'>
+                        <div id='overlay-1'>
                             <div className={!info ? 'hidden' : 'fixed inset-0 z-10 overflow auto none flex w-full bg-[#030200] backdrop-filter backdrop-blur-lg bg-opacity-70 flex-col justify-center items-center overflow-auto max-h-screen overscroll-none'}>
                                 <div onClick={handleClick} className='hidden-lg z-10'>
                                     {!info ? 'hidden' : <FaTimes className='absolute text-white text-2xl top-4 right-4 hover:text-pink-600' />}
@@ -87,24 +79,44 @@ const Projects = () => {
                                             K-12 Educational Game
                                         </h4>
                                         <p className='pb-4'>
-                                        Three minigames, each about a minute long, with a cumulative scoring system: 
-                                        Game 1: matching an animal in the estuary to its description. 
-                                        Game 2: playing as an animal from game 1, dodge predators and other animals while searching for food. 
-                                        Game 3: playing as a person, pick up trash using the right tools and receptacles.
-                                        Three minigames, each about a minute long, with a cumulative scoring system: 
-                                        Game 1: matching an animal in the estuary to its description. 
-                                        Game 2: playing as an animal from game 1, dodge predators and other animals while searching for food. 
-                                        Game 3: playing as a person, pick up trash using the right tools and receptacles.
-                                        Three minigames, each about a minute long, with a cumulative scoring system: 
-                                        Game 1: matching an animal in the estuary to its description. 
-                                        Game 2: playing as an animal from game 1, dodge predators and other animals while searching for food. 
-                                        Game 3: playing as a person, pick up trash using the right tools and receptacles.
-                                        Three minigames, each about a minute long, with a cumulative scoring system: 
-                                        Game 1: matching an animal in the estuary to its description. 
-                                        Game 2: playing as an animal from game 1, dodge predators and other animals while searching for food. 
-                                        Game 3: playing as a person, pick up trash using the right tools and receptacles.
+                                        Java game application consisting of three minigames, each about a minute long, with a cumulative scoring system: <br/>
+                                        Game 1: matching an animal in the estuary to its description. <br/>
+                                        Game 2: playing as an animal from game 1, dodge predators and other animals while searching for food. <br/>
+                                        Game 3: playing as a person, pick up trash using the right tools and receptacles.<br/>
                                         </p>
-                                        
+                                        <h4 className='text-2xl'>
+                                            App Features
+                                        </h4>
+                                        <ul className='list-disc pl-4 pb-4'>
+                                            <li>Cumulative scoring system</li>
+                                            <li>Adheres to MVC principles</li>
+                                        </ul>
+                                        <h4 className='text-2xl'>
+                                            Tech Stack
+                                        </h4>
+                                        <ul className='list-disc pl-4 pb-4'>
+                                            <li>Java</li>
+                                            <li>JavaFX</li>
+                                            <li>JUnit</li>
+                                        </ul>
+                                        <h4 className='text-2xl'>
+                                            Purpose
+                                        </h4>
+                                        <p className='pb-4'>
+                                            Created for the Delaware National Estuarine Research
+                                            Reserve to educate children about Delaware's estuarine system.<br/> 
+                                            Developed as part of a group project in an Intro to Software Engineering course (2019).
+                                        </p>
+                                        <h4 className='text-2xl'>
+                                            Responsibilities
+                                        </h4>
+                                        <ul className='list-disc pl-4 pb-4'>
+                                            <li>Programming in Java</li>
+                                            <li>Performing JUnit tests</li>
+                                            <li>Creating gameplay tutorial video</li>
+                                            <li>Creating and editing game graphics</li>
+                                            <li>Updating UML and Asana tasks</li>
+                                        </ul>
                                     </div>
                                 </div>
                             </div>
@@ -176,19 +188,42 @@ const Projects = () => {
                                             Vertical Scrolling Game
                                         </h6>
                                         <p className='pb-4'>
-                                        This game is a vertical endless falling game. Players can use keys to move left or right. 
-                                        Randomly generated bricks or obstacles will appear at the bottom along. 
-                                        Try to avoid the obstacle and get land on the safe brick and survive as long as you can. 
-                                        Score will be calculated according to both time and bricks you landed. Dungeon themed, similar to ns-shaft game.
-                                        This game is a vertical endless falling game. Players can use keys to move left or right. 
-                                        Randomly generated bricks or obstacles will appear at the bottom along. 
-                                        Try to avoid the obstacle and get land on the safe brick and survive as long as you can. 
-                                        Score will be calculated according to both time and bricks you landed. Dungeon themed, similar to ns-shaft game.
-                                        This game is a vertical endless falling game. Players can use keys to move left or right. 
-                                        Randomly generated bricks or obstacles will appear at the bottom along. 
-                                        Try to avoid the obstacle and get land on the safe brick and survive as long as you can. 
-                                        Score will be calculated according to both time and bricks you landed. Dungeon themed, similar to ns-shaft game.
+                                        A vertical endless falling game based on the theme of the 'Jack and the Beanstalk' fairytale.
+                                        The player can use arrow keys to move Jack left or right to dodge falling rocks and jump from leaf platforms.
+                                        Randomly generated rocks will fall from the top of the screen. Some leaf platforms will disintegrate
+                                        quickly after Jack lands on them.
+                                        The player should try to avoid the obstacles and land on the safe platforms and survive as long as they can. 
+                                        The final score is calculated according to both time and platforms the player landed on.
                                         </p>
+                                        <h4 className='text-2xl'>
+                                            App Features
+                                        </h4>
+                                        <ul className='list-disc pl-4 pb-4'>
+                                            <li>Cumulative scoring system</li>
+                                            <li>Randomly generated platforms & obstacles</li>
+                                        </ul>
+                                        <h4 className='text-2xl'>
+                                            Tech Stack
+                                        </h4>
+                                        <ul className='list-disc pl-4 pb-4'>
+                                            <li>JavaScript</li>
+                                            <li>HTML</li>
+                                            <li>CSS</li>
+                                        </ul>
+                                        <h4 className='text-2xl'>
+                                            Purpose
+                                        </h4>
+                                        <p className='pb-4'>
+                                            Developed as part of a group project in an Advanced Web Technologies course (2020).
+                                        </p>
+                                        <h4 className='text-2xl'>
+                                            Responsibilities
+                                        </h4>
+                                        <ul className='list-disc pl-4 pb-4'>
+                                            <li>CSS styling</li>
+                                            <li>Game assets</li>
+                                            <li>Creating graphics</li>
+                                        </ul>
                                     </div>
                                 </div>
                             </div>
@@ -266,16 +301,35 @@ const Projects = () => {
                                         As we re-acclimate to social interactions following the pandemic and other issues of racial injustices, 
                                         we must be aware of how our actions may affect those around us. We must do this because there is a growing deficiency 
                                         in understanding the perspectives of people of different races/ethnicities.  
-                                        As users, we need an application that allows us to practice mitigating this implicit racial bias in various social environments.
-                                        As we re-acclimate to social interactions following the pandemic and other issues of racial injustices, 
-                                        we must be aware of how our actions may affect those around us. We must do this because there is a growing deficiency 
-                                        in understanding the perspectives of people of different races/ethnicities.  
-                                        As users, we need an application that allows us to practice mitigating this implicit racial bias in various social environments.
-                                        As we re-acclimate to social interactions following the pandemic and other issues of racial injustices, 
-                                        we must be aware of how our actions may affect those around us. We must do this because there is a growing deficiency 
-                                        in understanding the perspectives of people of different races/ethnicities.  
-                                        As users, we need an application that allows us to practice mitigating this implicit racial bias in various social environments.
                                         </p>
+                                        <h4 className='text-2xl'>
+                                            App Features
+                                        </h4>
+                                        <ul className='list-disc pl-4 pb-4'>
+                                            <li>Interactive </li>
+                                            <li>3D Avatar with speech and facial movement capabilities</li>
+                                        </ul>
+                                        <h4 className='text-2xl'>
+                                            Tech Stack
+                                        </h4>
+                                        <ul className='list-disc pl-4 pb-4'>
+                                            <li>Unity</li>
+                                            <li>C# Scripts</li>
+                                        </ul>
+                                        <h4 className='text-2xl'>
+                                            Purpose
+                                        </h4>
+                                        <p className='pb-4'>
+                                            Developed as part of a group project in an Intro to Human-Computer Interaction course (2021).
+                                        </p>
+                                        <h4 className='text-2xl'>
+                                            Responsibilities
+                                        </h4>
+                                        <ul className='list-disc pl-4 pb-4'>
+                                            <li>Programming in C#</li>
+                                            <li>Linking C# Scripts for speech and facial movement to 3D Avatar</li>
+                                            <li>Unity Avatar Sampling</li>
+                                        </ul>
                                     </div>
                                 </div>
                             </div>
